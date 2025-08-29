faf7407e3a50d3be6cab61f48bc0ebf160ca3bc4
Derek <108168367+pixzcodes@users.noreply.github.com>
2025-08-29 11:43:44 -0400

    Refactor some routes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,10 @@
 import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("you got 10 notes");
-});
+app.use("/api/notes", notesRoutes);
 
-app.post("/api/notes", (req, res) => { 
-    res.status(201).json({message: "Note created successfully!"});
-});
-
-app.put("/api/notes/:id", (req, res) => { 
-    res.status(200).json({message: "Note updated successfully!"});
-});
-
-
-app.delete("/api/notes/:id", (req, res) => { 
-    res.status(200).json({message: "Note deleted successfully!"});
-});
 
 app.listen(5001, () => {
     console.log("Server started on port 5001");
