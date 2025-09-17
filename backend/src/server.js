aeaa94fac3ce2049c611403ca7e8a6844ee36d01
Derek <108168367+pixzcodes@users.noreply.github.com>
2025-09-17 15:37:19 -0400

    Front end home page mostly done

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,25 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("Server started on PORT:", PORT);
-    });
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
